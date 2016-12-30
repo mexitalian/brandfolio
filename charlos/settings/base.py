e5673cbab7d63524e751cc58a955ba0c4bed6a6b
Giovanni Carlo Marasco <mexitalian@gmail.com>
2016-12-29 18:11:31 -0600

    Adds email generation logic for ContactForm, currently only logs to console. Simple template created for the email.

--- charlos/settings/base.py
+++ charlos/settings/base.py
@@ -114,6 +114,15 @@ USE_L10N = True
 
 USE_TZ = True
 
+# Email settings
+# Temp: output the "email" to your console
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+DEFAULT_FROM_EMAIL = 'testing@example.com'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+EMAIL_USE_TLS = False
+EMAIL_PORT = 1025
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
