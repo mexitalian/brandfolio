4dbdb32362f6dd61af7afd4fc28578e7f45019c2
Giovanni Carlo Marasco <mexitalian@gmail.com>
2017-01-02 19:19:44 -0600

    Adds Dreamhost specific production settings. Upgrade Wagtail version.

--- charlos/settings/production.py
+++ charlos/settings/production.py
@@ -1,8 +1,22 @@
+# Settings for Dreamhost shared hosting
+
 from __future__ import absolute_import, unicode_literals
 
 from .base import *
 
-DEBUG = False
+with open('secret_key.txt') as f:
+  SECRET_KEY = f.read().strip()
+
+DEBUG = True
+
+ALLOWED_HOSTS = [
+  '.charlos.co.uk',
+  '.charlos.co.uk.'
+]
+
+STATIC_ROOT = '/home/mexitalian/sandbox.charlos.co.uk/public/'
+# BASE_URL = 'https://charlos.co.uk'
+
 
 try:
     from .local import *
